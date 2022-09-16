d5c45d1d6242d7eaf38f19deab99786b36b27d64
Yaimee <37310514+Yaimee@users.noreply.github.com>
2022-09-16 12:37:01 +0200

    second

--- src/main/java/rasmus/deletelater/DeleteLaterApplication.java
+++ src/main/java/rasmus/deletelater/DeleteLaterApplication.java
@@ -2,6 +2,8 @@ package rasmus.deletelater;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 
 @SpringBootApplication
 public class DeleteLaterApplication {
